f97fa7c0d9a5586fe78ae0dc079f3d23df032307
Phạm Hữu Bảo Chung <53622927+chungphb@users.noreply.github.com>
2021-04-13 11:15:25 +0700

    Update policy.h

--- include/ga/core/policy.h
+++ include/ga/core/policy.h
@@ -8,23 +8,23 @@ struct individual;
 struct default_encoding_method {};
 
 struct default_fitness_function {
-    double operator()(individual&);
+	double operator()(individual&);
 };
 
 struct default_initialization_method {
-    void operator()(population&);
+	void operator()(population&);
 };
 
 struct default_selection_method {
-    void operator()(population&);
+	void operator()(population&);
 };
 
 struct default_crossover_method {
-    void operator()(population&);
+	void operator()(population&);
 };
 
 struct default_mutation_method {
-    void operator()(population&);
+	void operator()(population&);
 };
 
 }
